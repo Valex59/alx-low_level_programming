2a8d46ab7ac9a210ca47e6b87d20cfa0cbf72c9f
Valex59 <valex4real5929@gmail.com>
2025-02-27 22:45:37 +0100

    a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,12 @@
 *
 * Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
-* Return: 0
+*
+* Return: Returns 0 (Success).
 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
